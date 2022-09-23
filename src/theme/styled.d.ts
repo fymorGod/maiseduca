b0eec59c1bf20bcfa28aaf867117e63c00d66285
fymorGod <mufinagamer@gmail.com>
2022-09-23 18:20:12 -0300

    limpando pastas

--- src/theme/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components';
-import colors from './colors';
-
-declare module 'styled-components' {
-    type ThemeType = typeof colors;
-
-    export interface DefaultTheme extends ThemeType {}
-}
